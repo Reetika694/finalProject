03752b1980ec451dd29ed2f62acc164d5d470418
Reetika694 <punamkunj694@gmail.com>
2025-07-17 19:35:08 +0530

    pushrd

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $conn = new mysqli(
   "sql303.infinityfree.com",  // Replace with your DB host
-  "if0_39486897",             // Your username
-  "fIFXmqiKVb",            // Your DB password
-  "if0_39486897_feedback"     // Your DB name
+  "",             // Your username
+  "",            // Your DB password
+  ""     // Your DB name
 );
 
 if ($conn->connect_error) {
